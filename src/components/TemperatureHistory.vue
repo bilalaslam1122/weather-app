cdf196a2bc7f2dac2b0c0c277ded865d833fb20c
bilalaslam1122 <bilalaslam6903@gmail.com>
2024-07-03 16:40:24 +0500

    fix: max-height for temperature history

--- src/components/TemperatureHistory.vue
+++ src/components/TemperatureHistory.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <h3 v-if="history.length > 0">Temperature History</h3>
-    <ul>
+    <ul class="history-list">
       <li v-for="(reading, index) in history" :key="index">
         {{ reading.dateTime }} - {{ reading.temperature }}°C
       </li>
@@ -14,3 +14,9 @@ export default {
   props: ["history"],
 };
 </script>
+<style scoped>
+.history-list {
+  max-height: 400px;
+  overflow-y: auto;
+}
+</style>
